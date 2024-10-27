294c6119154f405a0f39fe15ffdd8a59bba24f9d
isaac <is606337@ucf.edu>
2024-10-26 20:03:57 -0400

    Check protocol type

---
+++ include/enums/CCProtocol.h
@@ -0,0 +1,13 @@
+//
+// Created by Isaac on 10/26/24.
+//
+
+#ifndef CCPROTOCOL_H
+#define CCPROTOCOL_H
+
+enum CCProtocol {
+  MSI,
+  MESI
+};
+
+#endif //CCPROTOCOL_H
--- src/cc_model.cpp
+++ src/cc_model.cpp
@@ -1,7 +1,28 @@
+#include <algorithm>
 #include <iostream>
+#include <utility>
+#include "enums/CCProtocol.h"
 
-int main() {
-  std::cout << "Hello world!" << std::endl;
+CCProtocol getProtocol(std::string protocol)
+{
+  std::string _protocol = std::move(protocol);
+  std::transform(_protocol.begin(), _protocol.end(), _protocol.begin(), ::tolower);
+
+  if (_protocol == "msi") return MSI;
+  if (_protocol == "mesi") return MESI;
+
+  throw std::invalid_argument("Invalid protocol name!");
+}
+
+int main(int argc, char *argv[]) {
+  CCProtocol protocol = argc > 1 ? getProtocol(argv[1]) : throw std::invalid_argument("Protocol required!");
+
+  if (protocol == MSI)
+  {
+    std::cout << "YAY!" << std::endl;
+  }
+
+  std::cout << "Hello world! argc: " << argc << std::endl;
 
   return 0;
 }
